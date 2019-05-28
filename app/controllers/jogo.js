ce2c14c7aa7c404fae711e81653fd5bc9a5cae24
Your Name <you@example.com>
2019-05-28 14:12:48 -0300

    Implementando mvc

---
+++ app/controllers/jogo.js
@@ -0,0 +1,24 @@
+/*
+Controller jogo:
+Resonsável por:
+
+- manter uma instância do model
+- Pedir ao model para iniciar o jogo
+- Pedir ao model para lançar os dados
+- Pedir ao model paraencerrar o jogo
+- Pedir ao model para iniciar um novo jogo
+
+*/
+
+var model = require('../models/jogo');
+var jogoModel = undefined;
+
+module.exports.iniciar = function (application, req, res){
+
+	console.log('controller: iniciar');
+	console.log('controller: cria instância de jogo');
+	jogoModel = new model.Jogo();
+	console.log('controller: atualiza view - novoJogo');
+	res.render('novoJogo');
+}
+
